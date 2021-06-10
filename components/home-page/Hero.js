26558c58a4752873374d25e593bc38d631934a08
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-10 14:34:46 +0530

    some images added and hero component finished

--- components/home-page/Hero.js
+++ components/home-page/Hero.js
@@ -1,8 +1,20 @@
+import Image from "next/image";
+
+import classes from './Hero.module.css';
+
 const Hero = ()  => {
     return (
-        <div>
-            <h1>Hero</h1>
-        </div>
+        <section className={classes.hero}>
+            
+            <div className={classes.image}>
+                <Image src={'/images/site/bemal.jpg'}
+                       width={400} height={400} alt={'bemal'}/>
+            </div>
+            
+            <h1>Hi I'm bemal</h1>
+            <p>I blog about web dev , mobile dev, game dev and machine learning</p>
+            
+        </section>
     )
 }
 
