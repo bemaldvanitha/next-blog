aff6f4ed3341b34e1c487935f7c092992fbee97c
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-12 10:48:01 +0530

    all posts page finished

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,39 @@
+import AllPosts from "../../components/posts/AllPosts";
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'Getting Started with Nextjs',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJs is react framework',
+        date: '2020-02-10'
+    },
+    {
+        slug: 'getting-started-with-nextjs2',
+        title: 'Getting Started with Nextjs',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJs is react framework',
+        date: '2020-02-10'
+    },
+    {
+        slug: 'getting-started-with-nextjs3',
+        title: 'Getting Started with Nextjs',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJs is react framework',
+        date: '2020-02-10'
+    },
+    {
+        slug: 'getting-started-with-nextjs4',
+        title: 'Getting Started with Nextjs',
+        image: 'getting-started-nextjs.png',
+        excerpt: 'NextJs is react framework',
+        date: '2020-02-10'
+    }
+];
+
 const AllPostsPage = ()  => {
     return (
-        <div>
-            <h1>All Posts</h1>
-        </div>
+        <AllPosts posts={DUMMY_POSTS}/>
     )
 }
 
