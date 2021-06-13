1a0e79d53a01e2a2940ce5f8db2805379257a276
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-13 15:32:08 +0530

    single post page finish

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -3,21 +3,15 @@ import ReactMarkdown from "react-markdown";
 import classes from './PostContent.module.css';
 import PostHeader from "./PostHeader";
 
-const DUMMY_POST = {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with Nextjs',
-    image: 'getting-started-nextjs.png',
-    date: '2020-02-10',
-    content: '# This is a first post'
-};
-
 const PostContent = (props) => {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+    const { slug, image, title, content } = props.post;
+
+    const imagePath = `/images/posts/${slug}/${image}`;
 
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POST.title} image={imagePath}/>
-            <ReactMarkdown>{ DUMMY_POST.content }</ReactMarkdown>
+            <PostHeader title={title} image={imagePath}/>
+            <ReactMarkdown>{content}</ReactMarkdown>
         </article>
     )
 }
