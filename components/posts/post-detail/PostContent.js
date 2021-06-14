5ed89573eac78bfa9afcad30620d2c8ce17a0777
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-14 15:09:40 +0530

    holy shit rendering image with markdown added

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 
 import classes from './PostContent.module.css';
@@ -8,10 +9,36 @@ const PostContent = (props) => {
 
     const imagePath = `/images/posts/${slug}/${image}`;
 
+    const customRenderers = {
+        p(paragraph) {
+            const { node } = paragraph;
+
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+
+                console.log(`/images/posts/${slug}/${image.properties.src}`);
+
+                return (
+                    <div className={classes.image}>
+                        <Image
+                            src={`/images/posts/${slug}/${image.properties.src}`}
+                            alt={image.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                );
+            }
+
+            return <p>{paragraph.children}</p>;
+        },
+
+    };
+
     return (
         <article className={classes.content}>
             <PostHeader title={title} image={imagePath}/>
-            <ReactMarkdown>{content}</ReactMarkdown>
+            <ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
         </article>
     )
 }
