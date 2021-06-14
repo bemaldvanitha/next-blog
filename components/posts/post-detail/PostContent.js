0c2920deee0ec13003bb1ca3f30670ba347037b8
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-14 15:21:00 +0530

    holy shit rendering code snipt added

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import classes from './PostContent.module.css';
 import PostHeader from "./PostHeader";
@@ -32,7 +34,17 @@ const PostContent = (props) => {
 
             return <p>{paragraph.children}</p>;
         },
-
+        code(code) {
+            const { className, children } = code;
+            const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+            return (
+                <SyntaxHighlighter
+                    style={atomDark}
+                    language={language}
+                    children={children}
+                />
+            );
+        },
     };
 
     return (
