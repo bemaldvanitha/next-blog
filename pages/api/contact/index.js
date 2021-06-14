c6199eb1dbc0508135babdcf1eb97d24e1a8c16d
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-14 15:36:46 +0530

    api route added

---
+++ pages/api/contact/index.js
@@ -0,0 +1,19 @@
+const handler = (req,res) => {
+    if(req.method === 'POST'){
+        const { email, name, message } = req.body;
+
+        if(!email || !email.includes('@') || !name || name.trim() === '' || !message || message.trim() === ''){
+            return res.status(422).json({ message: 'invalid input' });
+        }
+
+        const newMessage = {
+            email: email,
+            name: name,
+            message: message
+        }
+
+        return res.status(201).json({ message: 'successfully added', data: message });
+    }
+}
+
+export default handler;
