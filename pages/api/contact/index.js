0b8ca4bac14a2a7318446381a8ed950b3bfb2f8f
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-06-16 14:30:06 +0530

    connect to db and finish api route

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -1,4 +1,6 @@
-const handler = (req,res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req,res) => {
     if(req.method === 'POST'){
 
         const { email, name, message } = req.body;
@@ -12,6 +14,28 @@ const handler = (req,res) => {
             name: name,
             message: message
         }
+
+        let client;
+
+        try{
+            client = await MongoClient.connect('mongodb+srv://bemaldvanitha:Bemal123@devconnector.2jyrs.mongodb.net/nextblog?retryWrites=true&w=majority');
+
+        }catch (err){
+            return res.status(500).json({ message: 'could not connect to db' });
+        }
+
+        const db = client.db();
+
+        try{
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+
+        }catch (err){
+            await client.close();
+            return res.status(500).json({ message: 'store message fail' });
+        }
+
+        client.close();
 
         return res.status(201).json({ message: 'successfully added', data: newMessage });
     }
